3bff0187932bf1a46ffacfc2f7348456ab00a3c2
OggyWoppy36 <ethan.028355@gmail.com>
2026-08-05 13:03:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,22 @@ const app = new App({
 });
 
 app.command("/playgame-ping", async ({ command, ack, respond }) => {
+    console.log(command)
     const start = Date.now()
     await ack();
     const latency = Date.now() - start;
     await respond({ text: `Pong!\nLatency: ${latency}ms` });
 });
+
+app.command("/playgame-help", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text: `Available Commands:
+    /playgame-ping - Check bot latency
+    /playgame-catfact - Get a cat fact`,
+  });
+});
+
 
 (async () => {
     await app.start();
