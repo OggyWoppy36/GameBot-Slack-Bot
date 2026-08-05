9c03359dcc9404e8f30dd58f652fbdec7e603fe8
OggyWoppy36 <ethan.028355@gmail.com>
2026-08-05 14:24:40 -0500

    added some commands
    trying a few commands to get a feel for api calls and syntax

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const axios = require("axios");
+const brawlToken = process.env.BRAWL_API_JWT;
 
 const { App } = require("@slack/bolt")
 
@@ -6,6 +8,14 @@ const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     appToken: process.env.SLACK_APP_TOKEN,
     socketMode: true
+});
+
+const brawlAPI = axios.create({
+  baseURL: "https://api.brawlstars.com/v1",
+  headers: {
+    'Authorization': `Bearer ${brawlToken}`,
+    'Accept': 'application/json'
+  }
 });
 
 app.command("/playgame-ping", async ({ command, ack, respond }) => {
@@ -25,6 +35,49 @@ app.command("/playgame-help", async ({ ack, respond }) => {
   });
 });
 
+app.command("/playgame-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+app.command("/playgame-freebies", async ({ command, ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://www.gamerpower.com/api/giveaways?type=game&sort-by=value");
+    //console.log(response);
+    text = `${response.data.length} games found: \n`;
+    for (let i=0; i<response.data.length; i++) {
+        text += `${response.data[i].title} ${response.data[i].open_giveaway}\n`
+    }
+    await respond({ text: `${text}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a free games." });
+  }
+});
+
+
+app.command("/playgame-brawlstars", async ({ command, ack, respond }) => {
+    await ack();
+    tag = command.text
+    tag = "%23" + tag.substring(1)
+    console.log(tag)
+    try {
+        const response = await brawlAPI.get(`/players/${tag}`);
+        console.log(response)
+        await respond({ text: `Your Brawl Stars Trophies:\n${response.highestTrophies}` });
+    } catch (err) {
+        console.log(tag);
+        console.log(err);
+        await respond({ text: "Failed to fetch your trophies." });
+    }
+});
 
 (async () => {
     await app.start();
