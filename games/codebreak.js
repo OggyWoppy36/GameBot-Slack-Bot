6c97b3e2ac1960403389b8aaa8d0658152a16cd9
OggyWoppy36 <ethan.028355@gmail.com>
2026-08-08 01:25:47 -0500

    Update codebreak.js

--- games/codebreak.js
+++ games/codebreak.js
@@ -5,6 +5,7 @@ function register(app, store) {
         await ack();
         const userId = command.userId;
         const input = command.text.trim();
+        const showLocations = false;
 
         let game = store.getGame(GAME_TYPE, userId);
         
@@ -18,13 +19,23 @@ function register(app, store) {
                 text: `Codebreak functions:
                     \`/oggy-codebreak\` - prints this list of functions
                     \`/oggy-codebreak [1-3]\` - creates new game with selected difficulty
-                    \`/oggy-codebreak mode\` - switches mode between easy and hard. Easy shows locations of correct/missplaced guesses
+                    \`/oggy-codebreak [mode]\` - changes mode to "easy" or "hard". Easy shows locations of correct/missplaced guesses
                     \`/oggy-codebreak [guess]\` - make a guess using numbers 1-8. Make sure the guess is the correct length.
                     \`/oggy-codebreak quit\` - ends current game and reveals the answer
                     `
             })
         }
 
+        if (input === "quit") {
+            store.deleteGame(GAME_TYPE,userId);
+            return respond({ text: `Game quit. The code was ${game.code}. You had ${game.guesses.length} guesses and spent ${calcTime(game)} seconds.`})
+        } else if (input === "easy") {
+            showLocations = true;
+        } else if (input === "hard") {
+            showLocations = false;
+        } else if (input.test(/^[1-3]$/)) {
+            game = createGame(userId, parseInt(input));
+        }
 
         return respond({
             text: "wow"
@@ -52,6 +63,10 @@ function createGame(userId, difficulty) {
       finishedAt: null,
       status: "in_progress",
     };
+}
+
+function calcTime(game) {
+    return ((Date.now() - game.startedAt) / 1000.0).toFixed(1);
 }
 
 module.exports = { register };
