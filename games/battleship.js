61ce9c9691690dadbf140112c6a406beb23ce6cd
OggyWoppy36 <ethan.028355@gmail.com>
2026-08-07 23:28:56 -0500

    Update battleship.js

--- games/battleship.js
+++ games/battleship.js
@@ -137,8 +137,8 @@ function placeShips(size) {
 }
 
 function renderBoard(game) {
-  const letts = "A B C D E  F G H I J";
-  let out = "```\n    " + letts + "\n";
+  const letts = "A B C D  E F G H  I J";
+  let out = "```\n     " + letts + "\n";
 
   for (let r = 0; r < game.size; r++) {
     let row = String(r + 1).padStart(2, " ") + "  ";
