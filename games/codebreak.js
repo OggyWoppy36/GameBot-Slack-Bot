ff9230c6175117990db46f5bd2e4c0a4a3f2c131
OggyWoppy36 <ethan.028355@gmail.com>
2026-08-08 01:08:44 -0500

    Update codebreak.js

--- games/codebreak.js
+++ games/codebreak.js
@@ -11,16 +11,16 @@ function register(app, store) {
         if (input == "") {
             if (!game || game.status !== "in_progress") {
               return respond({
-                text: "Please start the game with a difficulty (1-3) Ex: ```/oggy-codebreak 2``` "
+                text: "Please start the game with a difficulty (1-3) Ex: `/oggy-codebreak 2` "
               });
             }
             return respond({
                 text: `Codebreak functions:
-                    \`\`\`/oggy-codebreak\`\`\` - prints this list of functions
-                    \`\`\`/oggy-codebreak [1-3]\`\`\` - creates new game with selected difficulty
-                    \`\`\`/oggy-codebreak mode\`\`\` - switches mode between easy and hard. Easy shows locations of correct/missplaced guesses
-                    \`\`\`/oggy-codebreak [guess]\`\`\` - make a guess using numbers 1-8. Make sure the guess is the correct length.
-                    \`\`\`/oggy-codebreak quit\`\`\` - ends current game and reveals the answer
+                    \`/oggy-codebreak\` - prints this list of functions
+                    \`/oggy-codebreak [1-3]\` - creates new game with selected difficulty
+                    \`/oggy-codebreak mode\` - switches mode between easy and hard. Easy shows locations of correct/missplaced guesses
+                    \`/oggy-codebreak [guess]\` - make a guess using numbers 1-8. Make sure the guess is the correct length.
+                    \`/oggy-codebreak quit\` - ends current game and reveals the answer
                     `
             })
         }
